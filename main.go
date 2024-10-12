94a9947b167f0273af52830439fa84bd9651b2a2
Inteoty <kittipong.tpy@gmail.com>
2024-10-12 20:08:48 +0700

    redirectory

--- main.go
+++ main.go
@@ -2,75 +2,34 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/encall/cpeevent-backend/src/db"
-	"github.com/gin-gonic/gin"
+	"github.com/encall/cpeevent-backend/src/routes"
+
 	"github.com/joho/godotenv"
 )
 
-func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	r := gin.Default()
-
-	// Ping test
-	r.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
-	})
-
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	// authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-	// 	"foo":  "bar", // user:foo password:bar
-	// 	"manu": "123", // user:manu password:123
-	// }))
-
-	/* example curl for /admin with basicauth header
-	   Zm9vOmJhcg== is base64("foo:bar")
-
-		curl -X POST \
-	  	http://localhost:8080/admin \
-	  	-H 'authorization: Basic Zm9vOmJhcg==' \
-	  	-H 'content-type: application/json' \
-	  	-d '{"value":"bar"}'
-	*/
-	// authorized.POST("admin", func(c *gin.Context) {
-	// 	user := c.MustGet(gin.AuthUserKey).(string)
-
-	// 	// Parse JSON
-	// 	var json struct {
-	// 		Value string `json:"value" binding:"required"`
-	// 	}
-
-	// 	if c.Bind(&json) == nil {
-	// 		db[user] = json.Value
-	// 		c.JSON(http.StatusOK, gin.H{"status": "ok"})
-	// 	}
-	// })
-
-	return r
-}
-
 func main() {
+	// Load environment variables from .env file
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Get MongoDB URI from environment
 	mongoURI := os.Getenv("MONGO_URI")
 	if mongoURI == "" {
 		log.Fatal("MONGO_URI is not set in .env file")
 	}
 
-	r := setupRouter()
-	db.ConnectMongoDB(mongoURI)
-	// Listen and Server in 0.0.0.0:8080
+	// Connect to MongoDB
+	client := db.ConnectMongoDB(mongoURI)
+	defer client.Disconnect(nil) // Ensure the MongoDB client is disconnected at shutdown
+
+	// Set up routes
+	r := routes.SetupRouter(client)
+
+	// Start the Gin server on port 8080
 	r.Run(":8080")
 }
