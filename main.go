b664ecc8ea4922b9de9dee91d5e1c81b21af9157
encall <apichataimimpat@gmail.com>
2024-10-12 18:09:50 +0700

    feat: add structure and mongodb

--- main.go
+++ main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"net/http"
 
+	"github.com/encall/cpeevent-backend/src/db"
 	"github.com/gin-gonic/gin"
 )
-
-var db = make(map[string]string)
 
 func setupRouter() *gin.Engine {
 	// Disable Console Color
@@ -18,17 +17,6 @@ func setupRouter() *gin.Engine {
 		c.String(http.StatusOK, "pong")
 	})
 
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
-		}
-	})
-
 	// Authorized group (uses gin.BasicAuth() middleware)
 	// Same than:
 	// authorized := r.Group("/")
@@ -36,10 +24,10 @@ func setupRouter() *gin.Engine {
 	//	  "foo":  "bar",
 	//	  "manu": "123",
 	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
+	// authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+	// 	"foo":  "bar", // user:foo password:bar
+	// 	"manu": "123", // user:manu password:123
+	// }))
 
 	/* example curl for /admin with basicauth header
 	   Zm9vOmJhcg== is base64("foo:bar")
@@ -50,25 +38,26 @@ func setupRouter() *gin.Engine {
 	  	-H 'content-type: application/json' \
 	  	-d '{"value":"bar"}'
 	*/
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
+	// authorized.POST("admin", func(c *gin.Context) {
+	// 	user := c.MustGet(gin.AuthUserKey).(string)
 
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
+	// 	// Parse JSON
+	// 	var json struct {
+	// 		Value string `json:"value" binding:"required"`
+	// 	}
 
-		if c.Bind(&json) == nil {
-			db[user] = json.Value
-			c.JSON(http.StatusOK, gin.H{"status": "ok"})
-		}
-	})
+	// 	if c.Bind(&json) == nil {
+	// 		db[user] = json.Value
+	// 		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	// 	}
+	// })
 
 	return r
 }
 
 func main() {
 	r := setupRouter()
+	db.ConnectMongoDB()
 	// Listen and Server in 0.0.0.0:8080
 	r.Run(":8080")
 }
