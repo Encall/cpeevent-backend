f049637a17f91a5e18e8015507da6b070bf8f6fe
encall <apichataimimpat@gmail.com>
2024-11-08 14:55:30 +0700

    feat: add update account info function

--- src/controllers/accountController.go
+++ src/controllers/accountController.go
@@ -2,8 +2,6 @@ package controllers
 
 import (
 	"context"
-	_ "fmt"
-	"log"
 	"net/http"
 	"time"
 
@@ -16,14 +14,14 @@ import (
 // var validate = validator.New()
 
 type UpdateAccountInfo struct { //You have to name the struct field according to the JSON attribute
-    FirstName   string `json:"firstName"`
-    LastName    string `json:"lastName"`
-    Year        int    `json:"year"`
-    Email       string `json:"email"`
-    PhoneNumber string `json:"phoneNumber"`
+	FirstName   string `json:"firstName"`
+	LastName    string `json:"lastName"`
+	Year        int    `json:"year"`
+	Email       string `json:"email"`
+	PhoneNumber string `json:"phoneNumber"`
 }
 
-func GetInfo() gin.HandlerFunc{
+func GetInfo() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		userID, exists := c.Get("studentid")
@@ -50,25 +48,37 @@ func GetInfo() gin.HandlerFunc{
 	}
 }
 
-func UpdateInfo() gin.HandlerFunc{
-	return func(c *gin.Context){
+func UpdateInfo() gin.HandlerFunc {
+	return func(c *gin.Context) {
 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		userID, exists := c.Get("studentid")
 		var info UpdateAccountInfo
 		defer cancel()
-		
+
+		if !exists {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "User ID not found in context"})
+			return
+		}
 
 		if err := c.BindJSON(&info); err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
 
-		log.Println("Incoming JSON payload for UpdateInfo:", info)
-		err := userCollection.FindOne(ctx, bson.M{"studentid": userID}).Decode(&info)
+		updateInfo := bson.M{
+			"firstName":   info.FirstName,
+			"lastName":    info.LastName,
+			"year":        info.Year,
+			"email":       info.Email,
+			"phoneNumber": info.PhoneNumber,
+		}
 
-		//Unfinished
-		_ = err
-		_ = exists
+		result, err := userCollection.UpdateOne(ctx, bson.M{"studentid": userID}, bson.M{"$set": updateInfo})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
 
+		c.JSON(http.StatusOK, gin.H{"success": true, "result": result, "message": "Account info updated successfully"})
 	}
 }
